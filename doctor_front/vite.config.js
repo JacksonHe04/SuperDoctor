c496c21b45ee78ab3e4917bd3526e97f4b0b029b
Jackson <JacksonHe04@outlook.com>
2024-10-23 19:54:43 +0800

    部署准备

--- doctor_front/vite.config.js
+++ doctor_front/vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
-  base: '/',
+  base: '/SuperDcotor/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
